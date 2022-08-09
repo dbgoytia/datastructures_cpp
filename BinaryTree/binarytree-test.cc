187a1950f2c729d898526272bcdc8900abd8f115
Diego <diego.canizales1995@gmail.com>
2022-08-09 08:22:01 -0500

    adding insert method.

--- BinaryTree/binarytree-test.cc
+++ BinaryTree/binarytree-test.cc
@@ -8,9 +8,30 @@ TEST(BinaryTreeTest, AssignsCorrectValueToNewBinaryTree)
   EXPECT_EQ(root.val, 1);
 }
 
-TEST(BinaryTreeTest, InsertsValueInTree)
+TEST(BinaryTreeTest, InsertValueInTreeInCorrectPosition)
 {
+      // 1
+      //   \
+      //     4
   TreeNode root = TreeNode(1);
   root.insert(4);
   EXPECT_EQ(root.right->val, 4);
+}
+
+TEST(BinaryTreeTest, InsertsValuesInTreeInCorrectPosition)
+{
+  //        5
+  //     /     \
+  //    3      12
+  //   /  \    /  \
+  //  1    4  6    20 
+
+  TreeNode root = TreeNode(5);
+  root.insert(3);
+  root.insert(12);
+  root.insert(1);
+  root.insert(4);
+  root.insert(6);
+  root.insert(20);
+  EXPECT_EQ(root.right->right->val, 20);
 }
--- BinaryTree/binarytree.cc
+++ BinaryTree/binarytree.cc
@@ -2,8 +2,11 @@
 
 #include "binarytree.h"
 
-using namespace std;
-
+/**
+ * @brief Construct a new Tree Node:: Tree Node object
+ * 
+ * @param x integer to initialize the struct
+ */
 TreeNode::TreeNode(int x)
 {
     val = x;
@@ -11,8 +14,48 @@ TreeNode::TreeNode(int x)
     right = NULL;
 }
 
+/**
+ * @brief Inserts a new value in the Binary Tree
+ *
+ * @param val integer to insert in the binary tree
+ * @return void
+ */
 void TreeNode::insert(int val)
 {
     TreeNode * n = new TreeNode(val);
-    this->left = n;
+    
+    std::cout << "Traversing" << std::endl;
+    std::cout << this->val << std::endl;
+    std::cout << "Inserting" << std::endl;
+    std::cout << val << std::endl;
+
+    if( this->val > val )
+    {
+        // Should insert left
+        if( this->left == NULL)
+        {
+            std::cout << "Insert left" << std::endl;
+            // If there's nothing insert left
+            this->left = n;
+        } else {
+            std::cout << "Go left recursively" << std::endl;
+            // If there's something on the left, recursively
+            // go to left node and repeat.
+            this->left->insert(val);
+        }
+        
+    } else {
+        // Should insert right
+        if ( this->right == NULL )
+        {
+            std::cout << "Insert right" << std::endl;
+            // If there's nothing insert right
+            this->right = n;
+        } else {
+            std::cout << "Go right recursively" << std::endl;
+            // If there's something on the right, recursively
+            // go to right node and repeat.
+            this->right->insert(val);
+        }
+    }
 }
